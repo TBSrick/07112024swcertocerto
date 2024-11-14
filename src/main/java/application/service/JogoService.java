e3b3141d11441cd0f7a959580d02f36740c4ba98
TBSrick <tbsrick1@gmail.com>
2024-11-14 17:52:17 +0000

    salve 3

--- src/main/java/application/service/JogoService.java
+++ src/main/java/application/service/JogoService.java
@@ -1,10 +1,15 @@
 package application.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import application.model.Jogo;
 import application.record.JogoDTO;
+import application.record.JogoListDTO;
 import application.repository.JogoRepository;
 
 @Service
@@ -12,8 +17,24 @@ public class JogoService {
     @Autowired
     private JogoRepository jogoRepo;
 
+    public Iterable<JogoListDTO> getAllOnlyTitulo() {
+        return jogoRepo.findAll().stream().map(JogoListDTO::new).toList();
+    }
+
     public Iterable<JogoDTO> getAll() {
         return jogoRepo.findAll().stream().map(JogoDTO::new).toList(); //Lista todos 
+    }
+
+    public JogoDTO getOnebyId(long id) {
+        Optional<Jogo> resultado = jogoRepo.findById(id);
+        if(resultado.isEmpty()) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Jogo não encontrado"
+            );
+
+
+        }
+       return new JogoDTO(resultado.get());
     }
 
     public JogoDTO add(JogoDTO jogo) {
